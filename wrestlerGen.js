2c36da1c070fbdbeff2b51e9cbf302174fddbac4
Max B <Max.Bobowski@outlook.com>
2021-05-03 11:34:41 -0400

    Add array of wrestler objects

---
+++ wrestlerGen.js
@@ -0,0 +1,25 @@
+//Array of wrestler objects with name, and sayings
+const wrestlerArray = [{
+    name: 'Hulk Hogan',
+    sayings: ['Whatcha gonna do, when Hulkamania runs wild on you!', 'Hulkamania Is Running Wild, Brother', 'Jabroni', 
+            'God created the Heavens, he created the earth! He created all the Hulkamaniacs! Then, he created a set of 24-inch pythons, brother!,', 
+            'Well let me tell you something, brother!'],
+}, {
+    name: 'Macho Man',
+    sayings: ['I\'m the tower of power, too sweet to be sour.', 'I\'m funky like a monkey. Sky\'s the limit and space is the place!', 
+            'Too hot to handle, too cold to hold!', 'Expect the unexpected in the kingdom of madness!', 'Oooooh yeeeah!'],
+}, {
+    name: 'Stone Cold Steve Austin',
+    sayings: ['Austin 3:16 says I just whipped your @$$!', 'And that\'s the bottom line, cuz Stone Cold said so!', 
+            'Tune in next week, same Stone Cold time, same Stone Cold Channel!', 'McMahon 3:16 says \'I just pissed my pants!\'', 
+            'Does anybody have, a cold beer for Steve Austin?'],
+}, {
+    name: 'The Rock',
+    sayings: ['It doesn\'t matter what your name is', 'You run around here looking like a big, fat bowl of Fruity Pebbles!', 
+            'You want to go One on One with the Great One?', 'The Rock will Layeth the Smacketh Down all over your Candy Ass!', 
+            'If you smell what The Rock….. is….. cookin’!'],
+}, {
+    name: 'Chris Jericho',
+    sayings: ['You just made the list!', 'I\'m from Winnipeg, you idiot.', 'I give you the gift of Jericho. Drink it in, man.', 
+            'I am the Ayatollah of Rock and Rolla!', 'Will you please shut the hell up?'],
+}];
